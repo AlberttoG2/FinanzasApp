e4576c5566f706e32d06b3cbf16f8bbad26104e4
IvanMtz <17610520@utgz.edu.mx>
2021-04-03 15:59:58 -0600

    proveedores(Form)

--- src/app/views/menu/egresos/proveedores/form-proveedores/form-proveedores.page.ts
+++ src/app/views/menu/egresos/proveedores/form-proveedores/form-proveedores.page.ts
@@ -13,26 +13,44 @@
 export class FormProveedoresPage implements OnInit {
 
   public razonSocialCombo: _combo[];
-  @Input() nombre;
-  @Input() pais;
+  public monedaCombo: _combo[];
+  @Input() data;
   public formulario: FormGroup;
   submit: any;
   constructor( private modalCtrl: ModalController, private restService: RestService ) { }
 
   ngOnInit() {
     this.restService.combo<_combo[]>({id: 'RazonSocial'}, 'comboController').subscribe(result => this.razonSocialCombo = result);
-
+    this.restService.combo<_combo[]>({id: 'Divisa'}, 'comboController').subscribe(result => this.monedaCombo = result);
     this.formulario = this.restService.buildForm({
+      id: ['', Validators.required],
       razonSocial: ['', Validators.required],
-      fecha: ['', Validators.required]
+      nombre: ['', Validators.required],
+      rfc: ['', Validators.required],
+      moneda: ['', Validators.required],
+      nombreDeContacto: ['', Validators.required],
+      correoElectronico: ['', Validators.required],
+      direccion: ['', Validators.required],
+      telefono: ['', Validators.required],
+      estatus: ['', Validators.required],
+      tipo: ['', Validators.required]
     });
   }
 
   SalirConDatos() {
     this.modalCtrl.dismiss(this.formulario.value);
   }
-
   parchar(event: {component: IonicSelectableComponent, value: any }) {
       this.formulario.patchValue({razonSocial: event.value.id});
+  }
+  parche(event: {component: IonicSelectableComponent, value: any}) {
+    this.formulario.patchValue({moneda: event.value.id});
+  }
+  cancel() {
+    this.modalCtrl.dismiss();
+  }
+  save() {
+    console.log(this.formulario.value);
+    this.modalCtrl.dismiss(this.formulario.value);
   }
 }
