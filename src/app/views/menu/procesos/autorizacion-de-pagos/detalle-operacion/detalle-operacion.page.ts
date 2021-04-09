ca865be9797d10824b9db1b3f98a2bf7abb9bbfc
IvanMtz <17610520@utgz.edu.mx>
2021-04-09 02:03:34 -0500

    Modificacion detalle-operacion.page.html (error al mostrar informacion)

--- src/app/views/menu/procesos/autorizacion-de-pagos/detalle-operacion/detalle-operacion.page.ts
+++ src/app/views/menu/procesos/autorizacion-de-pagos/detalle-operacion/detalle-operacion.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, inject, Inject, OnInit} from '@angular/core';
 import {ModalController} from '@ionic/angular';
+import {_operacion} from '../../../../../interfaces/data.interface';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-detalle-operacion',
@@ -8,7 +10,10 @@ import {ModalController} from '@ionic/angular';
 })
 export class DetalleOperacionPage implements OnInit {
 
-  constructor(public modalController: ModalController) { }
+  constructor(public modalController: ModalController,
+              @inject(MAT_DIALOG_DATA) public data: _operacion) {
+    console.log(data);
+  }
 
   ngOnInit() {
   }
